4b2132c12ef3c22a4c7385bfa8ac804c567a0dec
Niklas H <niklas.haas@student.kit.edu>
2016-09-26 22:31:45 +0200

    First attempt for forum db. Script not executed yet

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,12 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
 import time
+import psycopg2
 
 ## Database connection
-DB = []
+
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,16 +17,28 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    #posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    #return posts
+    # Establish the connection inside the method
+    db = psycopg2.connect("dbname=forum")
+    c = db.cursor()
+    query = "select time,content from posts order by time desc;"
+    c.execute(query)
+    posts = c.fetchall()
+    db.close()
     return posts
 
 ## Add a post to the database.
-def AddPost(content):
+def AddPost(name, nickname):
     '''Add a new post to the database.
 
     Args:
       content: The text content of the new post.
     '''
     t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    c = db.cursor()
+    c.execute("insert into forum values (%s, %s, %s);"
+              %(t, name, nickname)
+    db.commit()
+    db.close()
