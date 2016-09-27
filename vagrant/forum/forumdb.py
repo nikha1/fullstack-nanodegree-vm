461af91adcd6e4c38743630b2ad3253dab0a6191
Niklas H <niklas.haas@student.kit.edu>
2016-09-27 21:21:48 +0200

    Modified the forumdb code. Inserted Input-sanitization. Performed output-sanitization also.

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -4,6 +4,7 @@
 
 import time
 import psycopg2
+import bleach
 
 ## Database connection
 
@@ -20,25 +21,28 @@ def GetAllPosts():
     #posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
     #posts.sort(key=lambda row: row['time'], reverse=True)
     #return posts
+
     # Establish the connection inside the method
     db = psycopg2.connect("dbname=forum")
     c = db.cursor()
-    query = "select time,content from posts order by time desc;"
+    query = "SELECT time,content FROM posts ORDER BY time DESC"
     c.execute(query)
-    posts = c.fetchall()
+    posts = [{'content': str(row[1]),
+              'time': str(row[0])}
+             for row in c.fetchall()]
     db.close()
     return posts
 
 ## Add a post to the database.
-def AddPost(name, nickname):
+def AddPost(content):
     '''Add a new post to the database.
 
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
+    #t = time.strftime('%c', time.localtime())
+    db = psycopg2.connect("dbname=forum")
     c = db.cursor()
-    c.execute("insert into forum values (%s, %s, %s);"
-              %(t, name, nickname)
+    c.execute("INSERT INTO posts (content) values (%s)",  (bleach.clean(content),))
     db.commit()
     db.close()
